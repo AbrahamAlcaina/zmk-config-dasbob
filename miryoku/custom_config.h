e877fd82f6e893198421dee05631d44fe7de024f
Abraham Alcaina <abraham.alcaina@gmail.com>
2024-05-05 19:55:46 +0200

    feat: use colemakdh on wsl

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
+#define MIRYOKU_ALPHAS=COLEMAKDHK
+#define MIRYOKU_EXTRA_COLEMAKDH
+// remap redo/undo 
+#define MIRYOKU_CLIPBOARD=WIN
