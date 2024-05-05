1573e6d4a170148eaa45afa2398c45b0265a4da6
Abraham Alcaina <abraham.alcaina@gmail.com>
2024-05-05 19:59:01 +0200

    feat: revert to qwerty for to facilitate the migration

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-#define MIRYOKU_ALPHAS=COLEMAKDHK
-#define MIRYOKU_EXTRA_COLEMAKDH
+// for the fist configuration avoid colemakdh
+#define MIRYOKU_ALPHAS=QWERTY
+/* #define MIRYOKU_ALPHAS=COLEMAKDHK */
+/* #define MIRYOKU_EXTRA_COLEMAKDH */
 // remap redo/undo 
 #define MIRYOKU_CLIPBOARD=WIN
