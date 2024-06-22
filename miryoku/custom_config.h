673f40cecc3e877450e06046ec90c3a8f3cdfa64
Abraham Alcaina <abraham.alcaina@gmail.com>
2024-06-22 14:27:00 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-// for the fist configuration avoid colemakdh
-#define MIRYOKU_ALPHAS=QWERTY
+//For the first configuration avoid colemakdh
+#define MIRYOKU_ALPHAS_QWERTY
 /* #define MIRYOKU_ALPHAS=COLEMAKDHK */
 /* #define MIRYOKU_EXTRA_COLEMAKDH */
 // remap redo/undo 
-#define MIRYOKU_CLIPBOARD=WIN
+#define MIRYOKU_CLIPBOARD_WIN
